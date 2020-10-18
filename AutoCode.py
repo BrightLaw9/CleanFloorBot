9cfdef9add3ea79eb9265be74bb9bf3f1c46ce26
SmartLawrence <46764863+SmartLawrence@users.noreply.github.com>
2020-10-18 11:14:30 -0400

    Add movement + sensors procedure
    Get sensor values and create automated movement logic.

--- AutoCode.py
+++ AutoCode.py
@@ -3,7 +3,37 @@ from BrickPi import *
 BrickPiSetup()
 BrickPi.SensorType[PORT_1] = EV3_GYRO_ABS_DPS
 BrickPi.SensorType[PORT_2] = NXT_ULTRASONIC
-BrickPi.MotorEnable[PORT_A] = 1
-BrickPi.MotorEnable[PORT_D] = 1
+BrickPi.MotorEnable[PORT_A] = 1 #Left
+BrickPi.MotorEnable[PORT_D] = 1 #Right
 
-BrickPiSetupSensors()
+BrickPiSetupSensors() 
+
+def move(speedLeft, speedRight): 
+  BrickPi.MotorSpeed[PORT_A] = speedLeft
+  BrickPi.MotorSpeed[PORT_D] = speedRight 
+
+def new_lane(): 
+  while abs_angle < 90: 
+        move(-75, 75)
+
+while True: 
+  BrickPiUpdateValues() 
+  distance = BrickPi.Sensor[PORT_2] 
+  abs_angle = BrickPi.Sensor[PORT_1] 
+ 
+  
+  #Check if approaching a wall 
+  #Is the measurement done in cm?  
+  if distance < 1000:  
+     new_lane()
+  #Realigning the robot to continue in a straight line
+    elif abs_angle >= 1: 
+      move(-75, 75)
+    elif abs_angle <= 1: 
+      move(75, -75)
+        
+     
+    
+  
+  
+  
