0da2a8072162e3d1cc9b991a1c058f027d5c2750
BrightLaw9 <46764863+BrightLaw9@users.noreply.github.com>
2022-03-21 14:03:06 -0400

    Update movement.py

--- movement.py
+++ movement.py
@@ -1,1 +1,12 @@
 # Functions: getLeftEncoder(), getRightEncoder()
+
+from BrickPi import * 
+
+class Movement: 
+  
+  def __init__( self ): 
+    pass 
+  
+  def getLeftEncoder( self ): 
+    # OR nMotorEncoder[]
+    return BrickPi.Encoder[leftMotor]
